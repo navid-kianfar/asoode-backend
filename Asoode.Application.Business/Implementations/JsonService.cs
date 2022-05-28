61b724a09cb696a5784ea3dea00dc573e6b07745
Navid Kianfar <nvd.kianfar@gmail.com>
2022-05-29 00:48:08 +0300

    json serializer

--- Asoode.Application.Business/Implementations/JsonService.cs
+++ Asoode.Application.Business/Implementations/JsonService.cs
@@ -1,16 +1,11 @@
 using Asoode.Application.Core.Contracts;
+using Utf8Json;
 
 namespace Asoode.Application.Business.Implementations;
 
 internal class JsonService : IJsonService
 {
-    public T Deserialize<T>(string json)
-    {
-        throw new NotImplementedException();
-    }
+    public T Deserialize<T>(string json) => JsonSerializer.Deserialize<T>(json);
 
-    public string Serialize(object obj)
-    {
-        throw new NotImplementedException();
-    }
+    public string Serialize(object obj) => JsonSerializer.ToJsonString(obj);
 }
