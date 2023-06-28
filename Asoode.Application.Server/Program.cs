8c9a7669a85a56b1bfdcad8d7155ed9c59458b1c
Navid Kianfar <navid.kianfar@outlook.com>
2023-06-28 16:37:10 +0300

    service wiring

---
+++ Asoode.Application.Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using Asoode.Shared.Abstraction.Helpers;
+
+namespace Asoode.Application.Server;
+
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.Title = "Asoode.Application.Server";
+        EnvironmentHelper.Configure();
+        var appIp = EnvironmentHelper.Get("APP_IP")!;
+        var appPort = EnvironmentHelper.Get("APP_PORT")!;
+        var builder = WebApplication.CreateBuilder(args);
+        if (!EnvironmentHelper.IsTest())
+            builder.WebHost.UseUrls($"http://{appIp}:{appPort}");
+        builder.Services.RegisterApp();
+        var app = builder.Build();
+        app.UseAppServices();
+
+        if (EnvironmentHelper.IsTest()) await app.RunAsync();
+        else await app.RunAsync();
+    }
+}
