894cc5d54307027b7eb52c63260ad3c409345fc9
Navid Kianfar <navid.kianfar@outlook.com>
2023-06-29 23:29:39 +0300

    BlogRepository.cs #1

--- Asoode.Shared.Database/Repositories/BlogRepository.cs
+++ Asoode.Shared.Database/Repositories/BlogRepository.cs
@@ -6,6 +6,7 @@ using Asoode.Shared.Abstraction.Types;
 using Asoode.Shared.Database.Contexts;
 using Asoode.Shared.Database.Contracts;
 using Asoode.Shared.Database.Extensions;
+using Asoode.Shared.Database.Tables;
 using Microsoft.EntityFrameworkCore;
 
 namespace Asoode.Shared.Database.Repositories;
@@ -114,39 +115,144 @@ internal class BlogRepository : IBlogRepository
         }
     }
 
-    public Task<OperationResult<bool>> DeletePost(Guid userId, Guid id)
+    public async Task<OperationResult<bool>> DeletePost(Guid userId, Guid id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            throw new NotImplementedException();
+        }
+        catch (Exception e)
+        {
+            await _loggerService.Error(e.Message, "BlogRepository.DeletePost", e);
+            return OperationResult<bool>.Failed();
+        }
     }
 
-    public Task<OperationResult<bool>> EditPost(Guid userId, Guid id, BlogPostEditDto model, StorageItemDto[] files)
+    public async Task<OperationResult<bool>> EditPost(Guid userId, Guid id, BlogPostEditDto model, StorageItemDto[] files)
     {
-        throw new NotImplementedException();
+        try
+        {
+            throw new NotImplementedException();
+        }
+        catch (Exception e)
+        {
+            await _loggerService.Error(e.Message, "BlogRepository.EditPost", e);
+            return OperationResult<bool>.Failed();
+        }
     }
 
-    public Task<OperationResult<bool>> CreatePost(Guid userId, Guid id, BlogPostEditDto model, StorageItemDto[] files)
+    public async Task<OperationResult<bool>> CreatePost(Guid userId, Guid id, BlogPostEditDto model, StorageItemDto[] files)
     {
-        throw new NotImplementedException();
+        try
+        {
+            throw new NotImplementedException();
+        }
+        catch (Exception e)
+        {
+            await _loggerService.Error(e.Message, "BlogRepository.CreatePost", e);
+            return OperationResult<bool>.Failed();
+        }
     }
 
-    public Task<OperationResult<bool>> CreateBlog(Guid userId, BlogEditDto model)
+    public async Task<OperationResult<bool>> CreateBlog(Guid userId, BlogEditDto model)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var exists = await _context.Blogs.AnyAsync(b =>
+                b.Culture == model.Culture && b.Type == model.Type);
+            if (exists) return OperationResult<bool>.Duplicate();
+
+            await _context.Blogs.AddAsync(new Blog
+            {
+                Culture = model.Culture,
+                Description = model.Description,
+                Keywords = model.Keywords,
+                Title = model.Title,
+                Type = model.Type
+            });
+            await _context.SaveChangesAsync();
+            return OperationResult<bool>.Success(true);
+        }
+        catch (Exception e)
+        {
+            await _loggerService.Error(e.Message, "BlogRepository.CreateBlog", e);
+            return OperationResult<bool>.Failed();
+        }
     }
 
-    public Task<OperationResult<bool>> EditBlog(Guid userId, Guid id, BlogEditDto model)
+    public async Task<OperationResult<bool>> EditBlog(Guid userId, Guid id, BlogEditDto model)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var blog = await _context.Blogs.SingleOrDefaultAsync(b => b.Id == id);
+            if (blog == null) return OperationResult<bool>.NotFound();
+
+            var exists = await _context.Blogs.AnyAsync(b =>
+                b.Culture == model.Culture && b.Type == model.Type && b.Id != id);
+            if (exists) return OperationResult<bool>.Duplicate();
+
+            blog.Culture = model.Culture;
+            blog.Description = model.Description;
+            blog.Keywords = model.Keywords;
+            blog.Title = model.Title;
+            blog.Type = model.Type;
+            await _context.SaveChangesAsync();
+            return OperationResult<bool>.Success(true);
+        }
+        catch (Exception e)
+        {
+            await _loggerService.Error(e.Message, "BlogRepository.EditBlog", e);
+            return OperationResult<bool>.Failed();
+        }
     }
 
-    public Task<OperationResult<GridResult<PostDto>>> BlogPosts(Guid userId, Guid id, GridFilterWithParams<GridQuery> model)
+    public async Task<OperationResult<GridResult<PostDto>>> BlogPosts(Guid userId, Guid id, GridFilterWithParams<GridQuery> model)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var query = _context.BlogPosts
+                .Where(i => i.BlogId == id)
+                .OrderByDescending(i => i.Priority)
+                .ThenByDescending(i => i.CreatedAt);
+            return await DbHelper.GetPaginatedData(query, tuple =>
+            {
+                var (items, startIndex) = tuple;
+                return items.Select((i, index) =>
+                {
+                    var vm = i.ToDto();
+                    vm.Index = startIndex + index + 1;
+                    return vm;
+                }).ToArray();
+            }, model.Page, model.PageSize);
+        }
+        catch (Exception e)
+        {
+            await _loggerService.Error(e.Message, "BlogRepository.BlogPosts", e);
+            return OperationResult<GridResult<PostDto>>.Failed();
+        }
     }
 
-    public Task<OperationResult<GridResult<BlogDto>>> BlogsList(Guid userId, GridFilterWithParams<GridQuery> model)
+    public async Task<OperationResult<GridResult<BlogDto>>> BlogsList(Guid userId, GridFilterWithParams<GridQuery> model)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var query = _context.Blogs.OrderByDescending(i => i.CreatedAt);
+            return await DbHelper.GetPaginatedData(query, tuple =>
+            {
+                var (items, startIndex) = tuple;
+                return items.Select((i, index) =>
+                {
+                    var vm = i.ToDto();
+                    vm.Index = startIndex + index + 1;
+                    return vm;
+                }).ToArray();
+            }, model.Page, model.PageSize);
+        }
+        catch (Exception e)
+        {
+            await _loggerService.Error(e.Message, "BlogRepository.BlogsList", e);
+            return OperationResult<GridResult<BlogDto>>.Failed();
+        }
     }
 
     public async Task<OperationResult<BlogDto[]>> AllBlogs(string culture)
