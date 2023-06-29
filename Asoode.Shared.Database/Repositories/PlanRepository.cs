1e43003086f8e68696a7689bb88e5fcf73a55671
Navid Kianfar <navid.kianfar@outlook.com>
2023-06-29 22:40:45 +0300

    PlanRepository

--- Asoode.Shared.Database/Repositories/PlanRepository.cs
+++ Asoode.Shared.Database/Repositories/PlanRepository.cs
@@ -4,6 +4,8 @@ using Asoode.Shared.Abstraction.Dtos.Plan;
 using Asoode.Shared.Abstraction.Types;
 using Asoode.Shared.Database.Contexts;
 using Asoode.Shared.Database.Contracts;
+using Asoode.Shared.Database.Extensions;
+using Asoode.Shared.Database.Tables;
 using Microsoft.EntityFrameworkCore;
 
 namespace Asoode.Shared.Database.Repositories;
@@ -11,13 +13,14 @@ namespace Asoode.Shared.Database.Repositories;
 internal class PlanRepository : IPlanRepository
 {
     private readonly ILoggerService _loggerService;
-    private readonly WebsiteContext _context;
+    private readonly PremiumDbContext _context;
 
-    public PlanRepository(ILoggerService loggerService, WebsiteContext context)
+    public PlanRepository(ILoggerService loggerService, PremiumDbContext context)
     {
         _loggerService = loggerService;
         _context = context;
     }
+
     public async Task<OperationResult<PlanDto[]>> List()
     {
         try
@@ -41,7 +44,16 @@ internal class PlanRepository : IPlanRepository
     {
         try
         {
-            throw new NotImplementedException();
+            var query = await _context.Plans.OrderBy(i => i.Order)
+                .AsNoTracking()
+                .ToArrayAsync();
+            return OperationResult<SelectableItem<Guid>[]>.Success(
+                query.Select(i => new SelectableItem<Guid>
+                {
+                    Text = i.Title,
+                    Value = i.Id
+                }).ToArray()
+            );
         }
         catch (Exception e)
         {
@@ -54,7 +66,59 @@ internal class PlanRepository : IPlanRepository
     {
         try
         {
-            throw new NotImplementedException();
+            var plan = await _context.UserPlanInfo
+                .Where(i => i.UserId == id)
+                .OrderByDescending(i => i.CreatedAt)
+                .FirstAsync();
+
+            plan.Days = model.Days;
+            plan.Description = model.Description;
+            plan.Enabled = model.Enabled;
+            plan.Picture = model.Picture;
+            plan.Project = model.Project;
+            plan.Title = model.Title;
+            plan.Type = model.Type;
+            plan.Unit = model.Unit;
+            plan.Users = model.Users;
+            plan.AttachmentSize = model.AttachmentSize;
+            plan.CanExtend = model.CanExtend;
+            plan.ComplexGroup = model.ComplexGroup;
+            plan.Space = model.Space;
+            plan.FeatureBlocking = model.FeatureBlocking;
+            plan.FeatureCalendar = model.FeatureCalendar;
+            plan.FeatureChat = model.FeatureChat;
+            plan.FeatureFiles = model.FeatureFiles;
+            plan.FeatureKartabl = model.FeatureKartabl;
+            plan.FeatureObjectives = model.FeatureObjectives;
+            plan.FeaturePayments = model.FeaturePayments;
+            plan.FeatureRelated = model.FeatureRelated;
+            plan.FeatureReports = model.FeatureReports;
+            plan.FeatureSeasons = model.FeatureSeasons;
+            plan.FeatureShift = model.FeatureShift;
+            plan.FeatureTree = model.FeatureTree;
+            plan.FeatureVote = model.FeatureVote;
+            plan.FeatureWbs = model.FeatureWbs;
+            plan.OneTime = model.OneTime;
+            plan.PlanCost = model.PlanCost;
+            plan.SimpleGroup = model.SimpleGroup;
+            plan.WorkPackage = model.WorkPackage;
+            plan.AdditionalProjectCost = model.AdditionalProjectCost;
+            plan.AdditionalSpaceCost = model.AdditionalSpaceCost;
+            plan.AdditionalUserCost = model.AdditionalUserCost;
+            plan.FeatureComplexGroup = model.FeatureComplexGroup;
+            plan.FeatureCustomField = model.FeatureCustomField;
+            plan.FeatureRoadMap = model.FeatureRoadMap;
+            plan.FeatureSubTask = model.FeatureSubTask;
+            plan.FeatureTimeOff = model.FeatureTimeOff;
+            plan.FeatureTimeSpent = model.FeatureTimeSpent;
+            plan.FeatureTimeValue = model.FeatureTimeValue;
+            plan.AdditionalComplexGroupCost = model.AdditionalComplexGroupCost;
+            plan.AdditionalSimpleGroupCost = model.AdditionalSimpleGroupCost;
+            plan.AdditionalWorkPackageCost = model.AdditionalWorkPackageCost;
+            plan.FeatureGroupTimeSpent = model.FeatureGroupTimeSpent;
+
+            await _context.SaveChangesAsync();
+            return OperationResult<bool>.Success(true);
         }
         catch (Exception e)
         {
@@ -67,7 +131,55 @@ internal class PlanRepository : IPlanRepository
     {
         try
         {
-            throw new NotImplementedException();
+            var plan = await _context.Plans.SingleAsync(i => i.Id == id);
+            plan.Days = model.Days;
+            plan.Description = model.Description;
+            plan.Enabled = model.Enabled;
+            plan.Order = model.Order;
+            plan.Picture = model.Picture;
+            plan.Project = model.Project;
+            plan.Title = model.Title;
+            plan.Type = model.Type;
+            plan.Unit = model.Unit;
+            plan.Users = model.Users;
+            plan.AttachmentSize = model.AttachmentSize;
+            plan.CanExtend = model.CanExtend;
+            plan.ComplexGroup = model.ComplexGroup;
+            plan.DiskSpace = model.DiskSpace;
+            plan.FeatureBlocking = model.FeatureBlocking;
+            plan.FeatureCalendar = model.FeatureCalendar;
+            plan.FeatureChat = model.FeatureChat;
+            plan.FeatureFiles = model.FeatureFiles;
+            plan.FeatureKartabl = model.FeatureKartabl;
+            plan.FeatureObjectives = model.FeatureObjectives;
+            plan.FeaturePayments = model.FeaturePayments;
+            plan.FeatureRelated = model.FeatureRelated;
+            plan.FeatureReports = model.FeatureReports;
+            plan.FeatureSeasons = model.FeatureSeasons;
+            plan.FeatureShift = model.FeatureShift;
+            plan.FeatureTree = model.FeatureTree;
+            plan.FeatureVote = model.FeatureVote;
+            plan.FeatureWbs = model.FeatureWbs;
+            plan.OneTime = model.OneTime;
+            plan.PlanCost = model.PlanCost;
+            plan.SimpleGroup = model.SimpleGroup;
+            plan.WorkPackage = model.WorkPackage;
+            plan.AdditionalProjectCost = model.AdditionalProjectCost;
+            plan.AdditionalSpaceCost = model.AdditionalSpaceCost;
+            plan.AdditionalUserCost = model.AdditionalUserCost;
+            plan.FeatureComplexGroup = model.FeatureComplexGroup;
+            plan.FeatureCustomField = model.FeatureCustomField;
+            plan.FeatureRoadMap = model.FeatureRoadMap;
+            plan.FeatureSubTask = model.FeatureSubTask;
+            plan.FeatureTimeOff = model.FeatureTimeOff;
+            plan.FeatureTimeSpent = model.FeatureTimeSpent;
+            plan.FeatureTimeValue = model.FeatureTimeValue;
+            plan.AdditionalComplexGroupCost = model.AdditionalComplexGroupCost;
+            plan.AdditionalSimpleGroupCost = model.AdditionalSimpleGroupCost;
+            plan.AdditionalWorkPackageCost = model.AdditionalWorkPackageCost;
+            plan.FeatureGroupTimeSpent = model.FeatureGroupTimeSpent;
+            await _context.SaveChangesAsync();
+            return OperationResult<bool>.Success(true);
         }
         catch (Exception e)
         {
@@ -80,7 +192,10 @@ internal class PlanRepository : IPlanRepository
     {
         try
         {
-            throw new NotImplementedException();
+            var plan = await _context.Plans.SingleAsync(i => i.Id == id);
+            plan.Enabled = !plan.Enabled;
+            await _context.SaveChangesAsync();
+            return OperationResult<bool>.Success(true);
         }
         catch (Exception e)
         {
@@ -93,7 +208,59 @@ internal class PlanRepository : IPlanRepository
     {
         try
         {
-            throw new NotImplementedException();
+            var plan = new Plan
+            {
+                Days = model.Days,
+                Description = model.Description,
+                Enabled = model.Enabled,
+                Order = model.Order,
+                Picture = model.Picture,
+                Project = model.Project,
+                Title = model.Title,
+                Type = model.Type,
+                Unit = model.Unit,
+                Users = model.Users,
+                AttachmentSize = model.AttachmentSize,
+                CanExtend = model.CanExtend,
+                ComplexGroup = model.ComplexGroup,
+                DiskSpace = model.DiskSpace,
+                FeatureBlocking = model.FeatureBlocking,
+                FeatureCalendar = model.FeatureCalendar,
+                FeatureChat = model.FeatureChat,
+                FeatureFiles = model.FeatureFiles,
+                FeatureKartabl = model.FeatureKartabl,
+                FeatureObjectives = model.FeatureObjectives,
+                FeaturePayments = model.FeaturePayments,
+                FeatureRelated = model.FeatureRelated,
+                FeatureReports = model.FeatureReports,
+                FeatureSeasons = model.FeatureSeasons,
+                FeatureShift = model.FeatureShift,
+                FeatureTree = model.FeatureTree,
+                FeatureVote = model.FeatureVote,
+                FeatureWbs = model.FeatureWbs,
+                OneTime = model.OneTime,
+                PlanCost = model.PlanCost,
+                SimpleGroup = model.SimpleGroup,
+                WorkPackage = model.WorkPackage,
+                AdditionalProjectCost = model.AdditionalProjectCost,
+                AdditionalSpaceCost = model.AdditionalSpaceCost,
+                AdditionalUserCost = model.AdditionalUserCost,
+                FeatureComplexGroup = model.FeatureComplexGroup,
+                FeatureCustomField = model.FeatureCustomField,
+                FeatureRoadMap = model.FeatureRoadMap,
+                FeatureSubTask = model.FeatureSubTask,
+                FeatureTimeOff = model.FeatureTimeOff,
+                FeatureTimeSpent = model.FeatureTimeSpent,
+                FeatureTimeValue = model.FeatureTimeValue,
+                AdditionalComplexGroupCost = model.AdditionalComplexGroupCost,
+                AdditionalSimpleGroupCost = model.AdditionalSimpleGroupCost,
+                AdditionalWorkPackageCost = model.AdditionalWorkPackageCost,
+                FeatureGroupTimeSpent = model.FeatureGroupTimeSpent
+            };
+
+            await _context.Plans.AddAsync(plan);
+            await _context.SaveChangesAsync();
+            return OperationResult<bool>.Success(true);
         }
         catch (Exception e)
         {
@@ -106,7 +273,17 @@ internal class PlanRepository : IPlanRepository
     {
         try
         {
-            throw new NotImplementedException();
+            var query = _context.Plans.OrderBy(i => i.Order);
+            return await DbHelper.GetPaginatedData(query, tuple =>
+            {
+                var (items, startIndex) = tuple;
+                return items.Select((i, index) =>
+                {
+                    var vm = i.ToDto();
+                    vm.Index = startIndex + index + 1;
+                    return vm;
+                }).ToArray();
+            }, model.Page, model.PageSize);
         }
         catch (Exception e)
         {
