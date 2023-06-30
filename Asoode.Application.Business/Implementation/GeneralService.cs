7d381006757d7c9de7d5c1b4d1b209568faebe04
Navid Kianfar <navid.kianfar@outlook.com>
2023-06-30 18:32:44 +0300

    general service

--- Asoode.Application.Business/Implementation/GeneralService.cs
+++ Asoode.Application.Business/Implementation/GeneralService.cs
@@ -4,8 +4,28 @@ namespace Asoode.Application.Business.Implementation;
 
 internal class GeneralService : IGeneralService
 {
+    private readonly Dictionary<string, Dictionary<string, object>> _repository;
+    
+    public GeneralService()
+    {
+        _repository = new Dictionary<string, Dictionary<string, object>>();
+        AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(t => t.GetTypes())
+            .Where(t =>
+                t.IsEnum &&
+                !string.IsNullOrEmpty(t.Namespace) &&
+                t.Namespace.Contains("Asoode.")
+            )
+            .ToList()
+            .ForEach(enm =>
+            {
+                var values = Enum.GetValues(enm).Cast<object>();
+                _repository[enm.Name] = values.ToDictionary(value => Enum.GetName(enm, value));
+            });
+    }
+
     public Dictionary<string, Dictionary<string, object>> Enums()
     {
-        throw new NotImplementedException();
+        return _repository;
     }
 }
